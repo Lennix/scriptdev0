d92337be6d994a6db430ec226d0214cfc3d47d9a
Lennix <lennix@mc-wow.eu>
2011-12-12 00:13:38 +0100

    Trap will be despawned at the beginning and then spawn

--- scripts/kalimdor/dire_maul/instance_dire_maul.cpp
+++ scripts/kalimdor/dire_maul/instance_dire_maul.cpp
@@ -185,9 +185,7 @@ void instance_dire_maul::OnObjectCreate(GameObject* pGo)
             m_uiGordokTributeGUID.push_back(pGo->GetObjectGuid());
             return;
         case GO_KNOT_THIMBLEJACK_CACHE:
-            break;
         case GO_FIXED_TRAP:
-            pGo->Delete();
             break;
         // West
         case GO_PYLON_1:
