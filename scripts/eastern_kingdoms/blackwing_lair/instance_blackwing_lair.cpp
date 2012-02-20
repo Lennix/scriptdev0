f8cef8cfbf423fb6192de0bf49a7517e6e8301c0
Lennix <lennix@mc-wow.eu>
2012-02-20 05:36:36 +0100

    Fixed savestream, reactivated supression aura

--- scripts/eastern_kingdoms/blackwing_lair/instance_blackwing_lair.cpp
+++ scripts/eastern_kingdoms/blackwing_lair/instance_blackwing_lair.cpp
@@ -297,7 +297,7 @@ void instance_blackwing_lair::SetData(uint32 uiType, uint32 uiData)
 
         std::ostringstream saveStream;
         saveStream << m_auiEncounter[0] << " " << m_auiEncounter[1] << " " << m_auiEncounter[2] << " "
-            << m_auiEncounter[3] << " " << m_auiEncounter[4] << m_auiEncounter[5] << " " 
+            << m_auiEncounter[3] << " " << m_auiEncounter[4] << " " << m_auiEncounter[5] << " " 
             << m_auiEncounter[6] << " " << m_auiEncounter[7] << " "
             << m_auiEncounter[8] << " " << m_auiEncounter[9];
 
